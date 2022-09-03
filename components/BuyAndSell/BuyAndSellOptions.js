8057c7ca0beb9c9588332586bda7a72929209df1
Prophet-Bestman <bestiproph@gmail.com>
2022-09-03 08:34:05 +0100

    Implemented UI for pay bills (utitlity) and fund wallet

---
+++ components/BuyAndSell/BuyAndSellOptions.js
@@ -0,0 +1,8 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+const BuyAndSellOptions = ({ options, setOption }) => {
+  return <Box></Box>;
+};
+
+export default BuyAndSellOptions;
